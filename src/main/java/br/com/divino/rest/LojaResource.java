1d3dee9c6b57174e2165ed80aa49dfaec7f4ba60
Divino <junincf@hotmail.com>
2018-06-15 03:02:16 -0300

    implementação do getlojas

--- src/main/java/br/com/divino/rest/LojaResource.java
+++ src/main/java/br/com/divino/rest/LojaResource.java
@@ -1,16 +1,20 @@
 package br.com.divino.rest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import br.com.divino.factory.LojaSingleton;
 import br.com.divino.model.Loja;
+import br.com.divino.model.Pedido;
 
 @Path("/loja")
 public class LojaResource {
@@ -19,6 +23,16 @@ public class LojaResource {
 	
 	public LojaResource() {
 		mapaLojas = LojaSingleton.getInstance().getMapaLojas();
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getLojas() {
+		List<Loja> lojas = new ArrayList<>(mapaLojas.values());
+		GenericEntity<List<Loja>> entity = new GenericEntity<List<Loja>>(lojas){};
+		
+		return Response.status(200).entity(entity).type(MediaType.APPLICATION_JSON).build();
 	}
 	
 	@GET
